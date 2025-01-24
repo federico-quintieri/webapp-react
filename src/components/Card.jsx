82510ae026c2f8bc281520b024e3563ea923aaaf
Federico Quintieri <fedequinti@gmail.com>
2025-01-24 20:29:05 +0100

    feat: tailwind added finally

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,3 +1,27 @@
-export function Card() {
-  return;
+const BASE_URL = "http://localhost:3000/";
+
+export function Card({ id, title, director, genre, release_year, image }) {
+  return (
+    <div
+      key={id}
+      className="card max-w-sm bg-white shadow-lg rounded-lg p-6 m-4 transition-transform transform hover:scale-105 hover:shadow-xl"
+    >
+      <h2 className="text-xl font-semibold text-gray-800">{title}</h2>
+      {/* Usa image per il path corretto */}
+      <img
+        src={`${BASE_URL}${image}`}
+        alt={`${title}`}
+        className="w-full object-contain rounded-lg mt-4"
+      />
+      <p className="text-gray-600 mt-2">
+        <span className="font-medium">Director:</span> {director}
+      </p>
+      <p className="text-gray-600 mt-1">
+        <span className="font-medium">Genre:</span> {genre}
+      </p>
+      <p className="text-gray-600 mt-1">
+        <span className="font-medium">Release Year:</span> {release_year}
+      </p>
+    </div>
+  );
 }
--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -19,8 +19,29 @@ export function CardList() {
     if (data && isSuccess) {
       // Setto al mio array state la data presa dal backend tramite chiamata api
       setMovies(data);
+      // console.log(movies);
     }
   }, [data, isSuccess]);
 
-  return;
+  return (
+    <div className="bg-gray-100 min-h-screen py-10">
+      <h1 className="text-3xl font-bold text-center text-gray-800 mb-8">
+        Movie Collection
+      </h1>
+      <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 px-4">
+        {/* Mappatura delle card */}
+        {movies.map((movie) => (
+          <Card
+            director={movie.director}
+            genre={movie.genre}
+            id={movie.id}
+            release_year={movie.release_year}
+            title={movie.title}
+            key={movie.id}
+            image={movie.image}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
